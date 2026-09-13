29ed350ac4489550bd20dd9e1ab8812a5fe682b7
nelvintan2025-oss <nelvin.tan.2025@smu.edu.sg>
2026-09-13 14:32:10 +0800

    New work

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -1,10 +1,19 @@
 <script setup>
 import { ref } from 'vue'
 const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
+const newItem = ref('')
 
 // TODO: Add Code Here
+function addItem(){
+  if (newItem.value.trim() !== ""){
+    items.value.push(newItem.value)
+    newItem.value = '' //clears the input box after adding
+  }
+}
 
-
+function removeItem(index){
+  items.value.splice(index,1)
+}
 </script>
 
 <template>
@@ -12,5 +21,16 @@ const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
 
-   
+    <ul>
+      <li v-for="(item,index) in items" :key="index">
+          {{ item }}
+          <button @click="removeItem(index)">Delete!</button>
+      </li>
+    </ul>
+
+    <!--Add items-->
+    <div>
+        <input v-model="newItem" type="text">
+        <button @click="addItem">Add!</button>
+    </div>
 </template>
